a6e2f73d440fdbe23626e7ade2dcc770e2bdbd93
Babawale Patrick <b.p.d.okare@rgu.ac.uk>
2021-03-19 18:54:38 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
   session_start(); 
 
   if (!isset($_SESSION['username'])) {
-  	$_SESSION['msg'] = "You must log in first";
+  	$_SESSION['msg'] = "You must log in first!";
   	header('location: login.php');
   }
   if (isset($_GET['logout'])) {
